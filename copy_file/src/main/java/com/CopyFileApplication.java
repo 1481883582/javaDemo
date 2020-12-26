e0a169cba026fce7aeb76eb12aa58d9683266e42
杨朝晖 <1481883582@qq.com>
2020-12-26 18:52:51 +0800

    打包有问题，提交复制外置jar文件

---
+++ copy_file/src/main/java/com/CopyFileApplication.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class CopyFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyFileApplication.class, args);
+
+		try {
+			FileUtil.copyConfig("/txt/测试.txt", "txt", "你好.txt");
+			FileUtil.copyConfig("/application.properties", "", "application.properties");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
