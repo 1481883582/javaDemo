00323a348305ba983909406f650494aa5e451e29
杨朝晖 <1481883582@qq.com>
2021-01-19 01:32:27 +0800

    聚合项目加入javaFx 制作托盘

--- java_fx/src/main/java/com/java_fx/JavaFxApplication.java
+++ java_fx/src/main/java/com/java_fx/JavaFxApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaFxApplication {
 
+//TODO mini版jre	./bin/jlink.exe --module-path jmods --add-modules java.base,java.desktop,java.xml,java.scripting,java.logging,jdk.unsupported --output minijre
 	public static void main(String[] args) {
 		//TODO JVM中一定要加   -Djava.awt.headless=false -Dfile.encoding=GBK
 		SpringApplication.run(JavaFxApplication.class, args);
